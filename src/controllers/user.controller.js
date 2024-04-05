314c9110ac445245d9eafc1d1c0795fea33e382f
dhanjay arya <dhanjayarya20@gmail.com>
2024-04-05 17:04:46 +0530

    add controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -21,15 +21,6 @@ const generateAccessAndRefreshTokens = async(userId)=>{
 }
 
 const registerUser=asyncHandler(async (req,res)=>{
-  //get user details from frontend 
-  // validation 
-  // check if user already exists 
-  // check the required image 
-  // upload them to cloudinary , avatar
-  // create user object -- create entry in db
-  // remove password and refresh token field from response
- // check for user creation 
- // return res
 
  const {fullName,email,username,password}=req.body
  console.log("useremail",email)
