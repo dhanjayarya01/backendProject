6a8fec1b4dd8ec5331a1c185787468c54f6b6fea
dhanjay arya <dhanjayarya20@gmail.com>
2024-04-12 07:13:46 +0530

    optimize MongoDB aggregation pipeline in getVideoById

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -213,7 +213,7 @@ const getVideoById =asyncHandler(async(req,res)=>{
     },
     {
         $project:{
-            "videoFile.url": 1,
+            "videoFile": 1,
             title: 1,
             description: 1,
             views: 1,
