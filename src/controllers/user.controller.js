ef91db455f78799d26fae77abfff798e42c178c4
dhanjay arya <dhanjayarya20@gmail.com>
2024-04-16 21:46:17 +0530

    typo : watchHistroy in usercontroller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -448,7 +448,7 @@ const getWatchHistory = asyncHandler(async(req, res) => {
 
   return res
   .status(200)
-  .json(new ApiResponse(200, user[0].watchHestory,"watchHistory fetched successfully "))
+  .json(new ApiResponse(200, user[0].watchHistory,"watchHistory fetched successfully "))
 })
 
 export {
