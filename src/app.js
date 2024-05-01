40cecf77f571974245a42f0fd120ba0786551768
dhanjay arya <dhanjayarya20@gmail.com>
2024-05-01 07:10:01 +0530

    added origin in cors

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ import cookieParser from "cookie-parser";
 const app= express()
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
+    origin: 'http://localhost:5173',
+    credentials:true,
+    domain: 'http://localhost:5173'
 }))
 
 app.use(express.json({limit:"16kb"}))
